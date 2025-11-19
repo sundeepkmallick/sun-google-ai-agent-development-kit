44bf6c9f8b54eafe1fcb3a3bb83d24bd09fbe660
Sundeep Kumar Mallick <sundeepkmallick@gmail.com>
2025-11-19 16:28:31 +0100

    Google AI ADK GitHub Reviewer
    My second AI agent using Google AI ADK that can review my GitHub repos

---
+++ my_agent_github_reviewer/agent.py
@@ -0,0 +1,23 @@
+from google.adk.agents import Agent
+from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPServerParams
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+
+GITHUB_TOKEN = os.environ['GIT_TOKEN_AI_ADK']
+
+root_agent = Agent(
+    model="gemini-2.5-pro",
+    name="github_agent",
+    instruction="Help users get information from GitHub",
+    tools=[
+        MCPToolset(
+            connection_params=StreamableHTTPServerParams(
+                url="https://api.githubcopilot.com/mcp/",
+                headers={
+                    "Authorization": f"Bearer {GITHUB_TOKEN}",
+                    "X-MCP-Toolsets": "all",
+                    "X-MCP-Readonly": "true"
+                },
+            ),
+        )
+    ],
+)
